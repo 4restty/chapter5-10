8a55fd49c6f5b01c6160598336d6e7eddef280d9
4restty <shota1072@gmail.com>
2021-09-07 13:45:17 +0000

    add destroy, use fallback, add books/index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,10 +8,12 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book)
-
   end
 
   def index
+    @books = Book.all
+    @user = User.find(current_user.id)
+    @new_book = Book.new
   end
 
   def show
@@ -20,7 +22,20 @@ class BooksController < ApplicationController
     @new_book = Book.new
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
