fa860e805aec1a936ccbf4946fcf53c2671934ec
4restty <shota1072@gmail.com>
2021-09-07 14:42:44 +0000

    Implement profile edit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5d53787e78d28a5b58bd5c75cab64b37d302d86b4cfa325b630161325117a2325e2a3fc4dfea3a560f717cc3a21746e4861feb6538016d7731ad6a625496ee3c'
